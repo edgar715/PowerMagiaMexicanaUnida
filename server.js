d18fec5d93176f04b94099410cb1c91712f1da83
edgarlti <greyzamora43@gmail.com>
2025-11-27 16:00:31 -0600

    agregamos el server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("creamos archivo");
+const Haxball = require('haxball-headless');
+
+Haxball.room.create({
+  name: "Mi Sala Render",
+  password: "",           // deja "" si no quieres contraseña
+  maxPlayers: 16,
+  public: true,
+  noPlayer: false,
+  token: "thr1.AAAAAGkoya1OrNKsIdxSFw.aArhywpn-yo"   // OBLIGATORIO: consigue tu token en https://www.haxball.com/headlesstoken
+}, (room) => {
+  console.log("Sala creada! Link:", room.link);
+
+  // Aquí puedes poner tus eventos (onPlayerJoin, etc.)
+  room.onPlayerJoin = (player) => {
+    console.log(player.name + " entró");
+  };
+});
